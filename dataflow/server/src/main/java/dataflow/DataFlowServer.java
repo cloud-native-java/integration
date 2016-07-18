00fd1db2a86da48e22aa6b924ca174651bb37aae
Josh Long <josh@joshlong.com>
2016-07-18 00:52:48 -0400

    code

--- dataflow/server/src/main/java/dataflow/DataFlowServer.java
+++ dataflow/server/src/main/java/dataflow/DataFlowServer.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.dataflow.server.EnableDataFlowServer;
 
-@EnableDataFlowServer
 // <1>
+@EnableDataFlowServer
 @SpringBootApplication
 public class DataFlowServer {
 
