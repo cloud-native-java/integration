76a8dc03f91e1c76bf9356b22a5aac355aa5ec0a
Josh Long <josh@joshlong.com>
2016-10-15 18:12:58 -0700

    testing, edge and integration merges
    Merge branch 'master' of git.atlas.oreilly.com:oreillymedia/spring-boot-essentials

--- activiti/src/test/java/com/example/EmailValidationServiceTest.java
+++ activiti/src/test/java/com/example/EmailValidationServiceTest.java
@@ -5,12 +5,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
-@SpringApplicationConfiguration(classes = DemoApplication.class)
+@SpringBootTest(classes = DemoApplication.class)
 public class EmailValidationServiceTest {
 
 	@Test
