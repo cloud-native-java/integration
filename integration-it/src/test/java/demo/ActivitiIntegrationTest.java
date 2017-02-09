83c2a0214bf37b062e16023169dcdd33bca17b2d
Josh Long <josh@joshlong.com>
2017-02-09 02:26:19 -0800

    retry

--- integration-it/src/test/java/demo/ActivitiIntegrationTest.java
+++ integration-it/src/test/java/demo/ActivitiIntegrationTest.java
@@ -12,11 +12,11 @@ import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.retry.support.RetryTemplate;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Map;
-import java.util.Optional;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = ClientConfiguration.class)
@@ -29,48 +29,51 @@ public class ActivitiIntegrationTest {
 			.basicAuthorization("operator", "operator")
 			.build();
 
+	private final RetryTemplate retryTemplate = new RetryTemplate();
+
 	private Log log = LogFactory.getLog(getClass());
 
 	@Test
 	public void testDistributedWorkflows() throws Throwable {
 
-		boolean endTimeExists = this.helper.uriFor("activiti-leader")
-				.map(al -> {
+		boolean endTimeExists =
+				this.helper.uriFor("activiti-leader")
+						.map(al -> {
 
-					ResponseEntity<Map<String, String>> entity =
-							this.restTemplate.exchange(al + "/start", HttpMethod.GET, null,
-									new ParameterizedTypeReference<Map<String, String>>() {
-									});
+							ResponseEntity<Map<String, String>> entity =
+									this.restTemplate.exchange(al + "/start", HttpMethod.GET, null,
+											new ParameterizedTypeReference<Map<String, String>>() {
+											});
 
-					Assert.assertEquals(entity.getStatusCode(), (HttpStatus.OK));
-					String processInstanceId = entity.getBody().get("processInstanceId");
+							Assert.assertEquals(entity.getStatusCode(), (HttpStatus.OK));
+							String processInstanceId = entity.getBody().get("processInstanceId");
 
-					int attempts = 0;
-					log.info("processInstanceId: " + processInstanceId);
-					boolean valid = false;
-					while (attempts++ < 30) {
-						try {
-							log.info("waiting 1 second.");
-							Thread.sleep(1000);
-							String url = al + "/history/historic-process-instances/" + processInstanceId;
-							Map<String, Object> instanceInformation =
-									this.restTemplate
-											.exchange(url, HttpMethod.GET, null, new ParameterizedTypeReference<Map<String, Object>>() {
-											})
-											.getBody();
+							int attempts = 0;
+							log.info("processInstanceId: " + processInstanceId);
+							boolean valid = false;
+							while (attempts++ < 30) {
+								try {
+									log.info("waiting 1 second.");
+									Thread.sleep(1000);
+									String url = al + "/history/historic-process-instances/" + processInstanceId;
+									Map<String, Object> instanceInformation =
+											this.restTemplate
+													.exchange(url, HttpMethod.GET, null, new ParameterizedTypeReference<Map<String, Object>>() {
+													})
+													.getBody();
 
-							if (instanceInformation.get("endTime") != null) {
-								valid = true;
-								break;
+									if (instanceInformation.get("endTime") != null) {
+										valid = true;
+										break;
+									}
+
+								} catch (InterruptedException e) {
+									throw new RuntimeException(e);
+								}
 							}
-
-						} catch (InterruptedException e) {
-							throw new RuntimeException(e);
-						}
-					}
-					return valid;
-				})
-				.orElse(false);
+							return valid;
+						})
+						.orElse(false);
 
 		Assert.assertTrue("the end time is nigh (or, at least, it should be)!",
 				endTimeExists);
