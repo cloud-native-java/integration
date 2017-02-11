c97bcc92dd837aa099d10e32ec6c9a666e2d5bca
Josh Long <josh@joshlong.com>
2017-02-10 21:40:24 -0800

    end to end works! :)

---
+++ integration-it/src/integration-test/java/integration/ShakyServiceIT.java
@@ -0,0 +1,9 @@
+package integration;
+
+/**
+ * @author <a href="mailto:josh@joshlong.com">Josh Long</a>
+ */
+// TODO make it so that the client actually calls the shaky service!
+
+public class ShakyServiceIT {
+}
